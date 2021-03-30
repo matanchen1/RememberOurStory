6c395dc0fd5338d26be848016f146f0712ca5b5d
Matan Chen <matanchen1>
2021-03-30 13:16:09 +0300

    #First Update

---
+++ app/src/main/java/com/example/rememberourstory/Heros.java
@@ -0,0 +1,8 @@
+package com.example.rememberourstory;
+
+public class Heros {
+    private String name;
+    private String urlSource;
+
+
+}
---
+++ app/src/main/java/com/example/rememberourstory/ItemListActivity.java
@@ -0,0 +1,8 @@
+package com.example.rememberourstory;
+
+import android.app.Activity;
+
+public class ItemListActivity extends Activity {
+
+
+}
