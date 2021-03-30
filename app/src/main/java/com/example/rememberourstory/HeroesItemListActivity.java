6fc9ab746ab4b60c14d107a13c257831771935f7
Matan Chen <matanchen1>
2021-03-30 15:52:29 +0300

    #Update Intro & list_item Screen

--- app/src/main/java/com/example/rememberourstory/HeroesItemListActivity.java
+++ app/src/main/java/com/example/rememberourstory/HeroesItemListActivity.java
@@ -1,5 +1,6 @@
 package com.example.rememberourstory;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,13 +10,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
 
 public class HeroesItemListActivity extends AppCompatActivity {
+    private Button returnBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heros_item_list);
 
+        returnBtn = findViewById(R.id.returBtn);
+        returnBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HeroesItemListActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
