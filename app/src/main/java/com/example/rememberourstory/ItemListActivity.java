3a9e40c4126294759455d039825fada6791d229a
Matan Chen <matanchen1>
2021-03-31 11:59:07 +0300

    #Update Intro & list_item Screen

--- app/src/main/java/com/example/rememberourstory/ItemListActivity.java
+++ app/src/main/java/com/example/rememberourstory/ItemListActivity.java
@@ -11,6 +11,6 @@ public class ItemListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_heros_item_list);
     }
 }
