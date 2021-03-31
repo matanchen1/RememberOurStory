172dde7c09cd07db8be0d52cd6af70ba5fa29017
Matan Chen <matanchen1>
2021-03-31 14:54:39 +0300

    #Update Intro & list_item Screen

--- app/src/main/java/com/example/rememberourstory/StoryActivity.java
+++ app/src/main/java/com/example/rememberourstory/StoryActivity.java
@@ -2,30 +2,71 @@ package com.example.rememberourstory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class StoryActivity extends AppCompatActivity {
-    Button shareToStoryBtn;
+    private Button shareToStoryBtn;
+    private Dialog popUpDialog;
+    private ImageView backBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
-        shareToStoryBtn = findViewById(R.id.letsVolunteer);
+        popUpDialog = new Dialog(this);
+        shareToStoryBtn = findViewById(R.id.openInstagramBtn);
         shareToStoryBtn.setOnClickListener(v -> {
             openInstagram();
+            managePopUp(v);
+        });
+        backBtn = findViewById(R.id.back_button);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StoryActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
         });
     }
+
     private void openInstagram() {
         Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.instagram.android");
         if (launchIntent != null) {
             startActivity(launchIntent);
         } else {
             Toast.makeText(this, "Oops! You don't have Instagram App.", Toast.LENGTH_LONG).show();
+            System.out.println( "Oops! You don't have Instagram App.");
         }
+
+    }
+    public void managePopUp(View v) {
+        TextView txtClose;
+        Button closeBtn;
+        popUpDialog.setContentView(R.layout.pop_up);
+        txtClose =(TextView) popUpDialog.findViewById(R.id.txtclose);
+        txtClose.setText("X");
+        closeBtn = (Button) popUpDialog.findViewById(R.id.closeBtn);
+        txtClose.setOnClickListener(v1 -> popUpDialog.dismiss());
+        closeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popUpDialog.dismiss();
+
+            }
+        });
+
+        popUpDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.BLUE));
+        popUpDialog.show();
     }
 
 
