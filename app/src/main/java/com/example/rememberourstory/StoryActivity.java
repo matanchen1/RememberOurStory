306c66bcf4877a5eacd7b1182aaf70b8e05d65f8
AdirTuval <adirtuval@gmail.com>
2021-03-31 13:16:15 +0300

    button open instagarm on click

--- app/src/main/java/com/example/rememberourstory/StoryActivity.java
+++ app/src/main/java/com/example/rememberourstory/StoryActivity.java
@@ -2,13 +2,31 @@ package com.example.rememberourstory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class StoryActivity extends AppCompatActivity {
+    Button shareToStoryBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
+        shareToStoryBtn = findViewById(R.id.letsVolunteer);
+        shareToStoryBtn.setOnClickListener(v -> {
+            openInstagram();
+        });
     }
+    private void openInstagram() {
+        Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.instagram.android");
+        if (launchIntent != null) {
+            startActivity(launchIntent);
+        } else {
+            Toast.makeText(this, "Oops! You don't have Instagram App.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
 }
