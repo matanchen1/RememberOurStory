b65ae08ce110fe27d5282b2944c731b42e39971b
Matan Chen <matanchen1>
2021-03-31 14:31:06 +0300

    #Update Intro & list_item Screen

--- app/src/main/java/com/example/rememberourstory/StoryActivity.java
+++ app/src/main/java/com/example/rememberourstory/StoryActivity.java
@@ -2,23 +2,21 @@ package com.example.rememberourstory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DownloadManager;
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class StoryActivity extends AppCompatActivity {
-    private Button downloadButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
-//        downloadButton= (Button) findViewById(R.id.button);
-//        downloadButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                downloadFile();
-//            }
-//        });
 
     }
+
+
 }
