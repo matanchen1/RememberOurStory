3b885f0775517b32ad3c29906d6fd73c95c4a578
Matan Chen <matanchen1>
2021-03-31 13:15:26 +0300

    #Update Intro & list_item Screen

--- app/src/main/java/com/example/rememberourstory/ItemListActivity.java
+++ app/src/main/java/com/example/rememberourstory/ItemListActivity.java
@@ -1,16 +1,39 @@
 package com.example.rememberourstory;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ItemListActivity extends AppCompatActivity {
+    private ImageView heroImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heros_item_list);
+        heroImage = findViewById(R.id.heroImage);
+//        heroImage.bringToFront();
+        heroImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("== My activity ===","OnClick is called");
+                Toast.makeText(v.getContext(), // <- Line changed
+                        "The favorite list would appear on clicking this icon",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
+
+
 }
